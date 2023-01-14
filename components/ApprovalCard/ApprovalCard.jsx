9a7af441c6de860b3095c1c77f7ab721da5cf95b
Yogesh Kumar <yogeshkumar180592@gmail.com>
2023-01-14 21:41:16 +0530

    Update ApprovalCard for more props

--- components/ApprovalCard/ApprovalCard.jsx
+++ components/ApprovalCard/ApprovalCard.jsx
@@ -1,15 +1,25 @@
 import React, { Children } from 'react'
+import Card from '../Card'
 import styles from '../../styles/Home.module.css';
 
-const ApprovalCard = (props) => {
+
+const ApprovalCard = ({
+   title,
+   info,
+   onApprove,
+   onDecline,
+  approveText = 'Approve',
+  declineText = 'Decline'
+}) => {
   return (
-    <div className={styles.card}>
-        <div  >{props.children}</div>
+    <Card>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.title}>{title}</div>
         <div className={styles.btns}>
-            <button>Approve</button>
-            <button>Decline</button>
+            <button onClick={onApprove}>{approveText}</button>
+            <button onClick={onDecline}>{declineText}</button>
         </div>
-    </div>
+    <Card/>
   )
 }
 
